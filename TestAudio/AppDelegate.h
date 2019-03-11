35f210fd0c9128a992a2750b08b8fa49ddeb955f
chenyuey <wodechangemail@gmail.com>
2019-03-11 11:51:44 +0800

    Initial Commit

---
+++ TestAudio/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TestAudio
+//
+//  Created by macbookpro on 2019/3/11.
+//  Copyright © 2019年 macbookpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ TestAudio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAudio
+//
+//  Created by macbookpro on 2019/3/11.
+//  Copyright © 2019年 macbookpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
